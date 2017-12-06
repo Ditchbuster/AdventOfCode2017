450cb7608b94739ca08d706e254a24e8bd940bff
Chris P <trioman008@hotmail.com>
2017-12-05 22:35:47 -0700

    day 6 done

---
+++ day06/day06b.py
@@ -0,0 +1,24 @@
+def state(parts):
+    return ''.join(str(parts))
+
+with open('input.txt') as f:
+    content = f.readlines()
+count = 0
+for line in content:
+    parts = [int(x) for x in line.split()]
+    states = []
+    curState = state(parts)
+    count = 0
+    while curState not in states:
+        states.append(curState)
+        blocks = max(parts)
+        maxIndex = parts.index(blocks)
+        parts[maxIndex] = 0
+        for i in range(1,blocks+1):
+            parts[(maxIndex+i)%len(parts)] += 1
+        curState = state(parts)
+        count += 1
+    print(count)
+    print(len(states) - states.index(curState))
+
+
